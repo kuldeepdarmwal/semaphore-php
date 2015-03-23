2180b768f003507e08bfeb81a030078097b95d8a
Ashish <ashishka@cybage.com>
2015-03-23 15:20:16 +0530

    Changes done

--- ecomm_project/BodyAddToCart.php
+++ ecomm_project/BodyAddToCart.php
@@ -10,14 +10,7 @@
 	$price=0;
 	$arra=[];	
 	$arra=array(explode("&",str_replace('%2F','/',(str_replace('%2C',',',urldecode(html_entity_decode($_SERVER['QUERY_STRING'])))))));
-	$array1 = $arra[0];
-	foreach ($array1 as $key =>$value) {
-		if($key == 0){
-			$array1[$key]= substr($value,18,-18);
-		} else {
-			$array1[$key] = substr($value,14,-14);
-		}
-	}
+	include_once "ForLoopDisplay.php";
 	echo '<tr>';
 	include_once "DisplayTable.php";
 echo "</tr>";
--- ecomm_project/BodyOrderSummery.php
+++ ecomm_project/BodyOrderSummery.php
@@ -15,14 +15,7 @@ $record=$obj->read_record($field, $table, $condition);
 $arra=[];
 $price=0;
 $arra=array(explode("&",str_replace('%2F','/',(str_replace('%2C',',',urldecode(html_entity_decode($_SESSION['key'])))))));
-	$array1 = $arra[0];
-	foreach ($array1 as $key =>$value) {
-		if ($key == 0){
-			$array1[$key]= substr($value,18,-18);
-		} else {
-			$array1[$key] = substr($value,14,-14); 
-		}
-	}
+include_once "ForLoopDisplay.php"
 echo '<tr>';
 include "DisplayTable.php";
 echo "</tr>";
